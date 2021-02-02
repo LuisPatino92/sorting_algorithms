37309f284f8a7577f181cf8ef4646bf60e499071
Lucho <2450@holbertonschool.com>
2021-02-02 11:54:30 -0500

    NULL array handled

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,20 +12,20 @@ void bubble_sort(int *array, size_t size)
 	int aux = 0;
 	size_t i, max_limit = size - 1;
 
-	do {
-		swapped = 'n';
-		for (i = 0; i < max_limit; i++)
-		{
-			if (*(array + i) > *(array + i + 1))
+	if (array && size > 0)
+		do {
+			swapped = 'n';
+			for (i = 0; i < max_limit; i++)
 			{
-				aux = *(array + i);
-				*(array + i) = *(array + i + 1);
-				*(array + i + 1) = aux;
-				print_array(array, size);
-				swapped = 'y';
+				if (*(array + i) > *(array + i + 1))
+				{
+					aux = *(array + i);
+					*(array + i) = *(array + i + 1);
+					*(array + i + 1) = aux;
+					print_array(array, size);
+					swapped = 'y';
+				}
 			}
-		}
-		max_limit--;
-	} while (swapped != 'n');
-
+			max_limit--;
+		} while (swapped != 'n');
 }
